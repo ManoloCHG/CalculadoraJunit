674c4bb19253bc1a86f71ca468c533a0169c9311
ManoloCHG <46519969+ManoloCHG@users.noreply.github.com>
2019-04-23 10:29:36 +0200

    Merge origin/master
    Conflicts:
    	src/com/pradopolixe/centipede/Main.java

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -10,29 +10,45 @@ package calculadora;
  * @author PC04
  */
 public class Calculadora {
-    
+    static double ans;
     public static double suma (double a , double b){
-        return a+b;
+        ans = a+b;
+        return ans;
     } 
     public static double resta (double a , double b){
-        return a-b;
+        ans = a-b;
+        return ans;
     }
     public static double multiplica (double a , double b){
-        return a*b;
+        ans = a*b;
+        return ans;
     }
     public static double divide (double a , double b){
-        return a/b;
+        ans = a/b;
+        return ans;
+    }
+    public static double suma2 (double a){
+        ans = a+ans;
+        return ans;
+    } 
+    public static double resta2 (double a){
+        ans = a-ans;
+        return ans;
+    }
+    public static double multiplica2 (double a){
+        ans = a*ans;
+        return ans;
+    }
+    public static double divide2 (double a){
+        ans = a/ans;
+        return ans;
     }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int res;
-        int suma;
-        int multiplica;
-        int divide;
-
+      
         System.out.println(divide(8,2));   
     }   
 }
